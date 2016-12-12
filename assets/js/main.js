c46d981d55c9608c7a25568841fc12c027976c7b
AxelGoetz <axel.goetz@hotmail.com>
2016-12-12 01:32:04 +0000

    Initial conversation

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,13 @@
+var id = 0;
+var message = "";
+var currentFunction;
+var chatContainer = document.getElementById('chat-container');
+
 init();
 
 function init() {
   initInput();
+  beginConversation();
 }
 
 function initInput() {
@@ -18,4 +24,81 @@ function checkVisibilityPlaceHolder() {
   } else {
     placeholder.style.visibility = 'visible';
   }
+}
+
+function beginConversation() {
+  currentFunction = beginConversation;
+  if(id === 0) {
+    message = "Hi there, welcome to Axel Goetz' personal website :-)";
+    animateResponse(1500);
+  } else if(id === 1) {
+    message = "I'm here to guide you around so if there is anything you need, just let me know!";
+    message += "</br>To get you started, here are some of the things that I can do:";
+    animateResponse(2000);
+  } else {
+    currentFunction = null;
+  }
+}
+
+function animateResponse(timeout) {
+  var loading = getLoading();
+  var loadingMessage = getMessage(loading, id, true, false);
+  chatContainer.innerHTML += loadingMessage;
+  setTimeout(replaceLoading, timeout);
+}
+
+function replaceLoading() {
+  var elem = document.getElementById(id);
+  elem.getElementsByClassName("chat-message")[0].innerHTML = message;
+  elem.getElementsByClassName("chat-bubble")[0].innerHTML += getMeta();
+  id++;
+  currentFunction();
+}
+
+/**
+ * Gets the HTML of a new message
+ * @param  {String}  text        Text in bubble
+ * @param  {String}  id          If you want to give your message an id
+ * @param  {Boolean} isLeft      If the chat appears on the left
+ * @param  {Boolean} includeMeta If you want to include meta
+ * @return {String}
+ */
+function getMessage(text, id, isLeft, includeMeta) {
+  var message = '<div class="message-wrapper" id="' + id + '">';
+  message += '<div class="chat-bubble ';
+
+  if(isLeft)  message += 'chat-left">';
+  else        message += 'chat-right">';
+
+  message += '<div class="chat-message">';
+  message += text;
+  message += '</div>';
+
+  if(includeMeta) {
+    message += getMeta(!isLeft);
+  }
+  message += '</div>';
+
+  return message;
+}
+
+/**
+ * You don't want to include the tag when sending
+ */
+function getMeta(includeTag) {
+  var message = '<div class="meta-data"><div class="chat-time">';
+  var date = new Date();
+  message += date.getHours() + ':' + date.getMinutes() + '</div>';
+  if(includeTag) {
+    message += '<i class="fa fa-check" aria-hidden="true"></i>';
+  }
+  message += '</div></div>';
+
+  return message;
+}
+
+function getLoading() {
+  var message = '<div class="loading"><div class="loading-dot"></div>';
+  message += '<div class="loading-dot"></div><div class="loading-dot"></div></div>';
+  return message;
 }
